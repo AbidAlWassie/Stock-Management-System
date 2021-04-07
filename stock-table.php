ab2fb935dc58daf4ffeb969630a74f444f3cf52b
Abid Al-Wasi <abidalwasi@outlook.com>
2021-04-08 05:05:03 +0600

    updated project

--- stock-table.php
+++ stock-table.php
@@ -10,62 +10,64 @@
 <body>
 
 <table class="table-sortable" id="stock">
-    <thead id="list-column">
-      <tr>
-        <th>Brand</th>
-        <th>Product Name</th>
-        <th>Color</th>
-        <th>Size</th>
-        <th>Price</th>
-        <th>Quantity</th>
-      </tr>
-    </thead>
+  <thead id="list-column">
+    <tr>
+      <th>Brand</th>
+      <th>Product Name</th>
+      <th>Color</th>
+      <th>Size</th>
+      <th>Price</th>
+      <th>Quantity</th>
+      <th>Product ID</th>
+    </tr>
+  </thead>
 
-<?php
-include("connect.php");
-error_reporting(0);
-$query = "SELECT * FROM products";
-$data = mysqli_query($connect, $query);
-$total = mysqli_num_rows($data); 
 
-if ($total!=0) {
-  echo "Table has records";
-} else {
-  echo "No records found";
-}
 
-?>
 
-    <tbody id="root-stock" class="list-items">
-      <tr>
-        <th>Berger</th>
-        <th>SPD</th>
-        <th>White</th>
-        <th>18 liter</th>
-        <th>520</th>
-        <th>1</th>
-      </tr>
-      <tr>
-        <th>Berger</th>
-        <th>SPD</th>
-        <th>White</th>
-        <th>18 liter</th>
-        <th>520</th>
-        <th>1</th>
-      </tr>
-    </tbody>
+  <tbody id="root-stock" class="list-items">
+    <?php
+    include("connect.php");
+    error_reporting(0);
+    $query = "SELECT * FROM products";
+    $data = mysqli_query($connect, $query);
+    $total = mysqli_num_rows($data); 
 
-    <tfoot>
-      <tr>
-      <th></th>
-      <th></th>
-      <th></th>
-      <th>Total</th>
-      <th id="result"></th>
-      <th></th>
-      </tr>
-    </tfoot>
-    </table>
+    if ($total!=0) {
+        while($result=mysqli_fetch_assoc($data)) 
+        {
+          echo 
+          "
+          <tr>
+          <td>".$result['Brand']."</td>
+          <td>".$result['Product Name']."</td>
+          <td>".$result['Color']."</td>
+          <td>".$result['Size']."</td>
+          <td>".$result['Price']."</td>
+          <td>".$result['Quantity']."</td>
+          <td>".$result['Product ID']."</td>
+          </tr>
+          ";
+        }
+    } 
+    else {
+      echo "No records found";
+    }
+  ?>
+  </tbody>
+
+  <tfoot>
+    <tr>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th>Total</th>
+    <th id="result"></th>
+    <th></th>
+    </tr>
+  </tfoot>
+  </table>
 
 
 

4fef5dc3bccb257e9d4b2e376bc606456055cd78
Abid Al-Wasi <abidalwasi@outlook.com>
2021-04-07 06:07:20 +0600

    updated project

--- connect.php
+++ connect.php
@@ -1,14 +1,16 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "Super Zero44";
-$dbname = "stock-table";
+$host = "localhost";
+$user = "root";
+$password = "";
+$db_name = "stock-table";
+$db_port = 3307;
 
-$connection = mysqli_connect($servername, $username, $password, $dbname);
+$connect = mysqli_connect($host, $user, $password, $db_name, $db_port);
 
-if ($connection) {
-  echo "Hello!";
+if ($connect) {
+  echo "Succesfully Connected!";
+} else {
+  echo "Connection Failed".mysqli_connect_error();
 }
-
 ?>

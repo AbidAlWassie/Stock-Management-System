440539559000e50c2e2496560127601c3bcdd421
Abid Al-Wasi <abidalwasi@outlook.com>
2021-05-25 20:28:28 +0600

    updated project

--- templates/footer.php
+++ templates/footer.php
@@ -2,27 +2,6 @@
 
   window.onload = () => {
 
-    var modal = document.getElementById("myModal");
-    var btnOpen = document.getElementById("btnModify");
-
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btnOpen.onclick = function() {
-      modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
 
     // var rootStock = document.getElementById("root-stock");
     // rootStock.insertAdjacentHTML("beforeend", htmlString);

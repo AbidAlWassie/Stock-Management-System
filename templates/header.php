3140be9defda9fb8622d860ff2a193250442390a
Abid Al-Wasi <abidalwasi@outlook.com>
2021-05-27 21:27:45 +0600

    updated project

--- templates/header.php
+++ templates/header.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 ?>
 <!DOCTYPE html>
-<html>
+<html class="<?php echo $theme ?>">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,6 +11,8 @@ error_reporting(0);
   <!-- <link rel="stylesheet" href="css/table.css"> -->
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/nav.css">
+  <link rel="stylesheet" href="css/simple-scrollbar.css">
+  <script src="js/simple-scrollbar.js"></script>
   <?php echo $form_styles ?>
   <?php echo $delete_styles ?>
   <!-- <link rel="stylesheet" href="css/form.css"> -->

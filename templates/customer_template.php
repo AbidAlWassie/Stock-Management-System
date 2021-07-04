84ab4e70067986daa8c71a653ac0e03d52f11899
Abid Al-Wasi <abidalwasi@outlook.com>
2021-07-04 15:14:14 +0600

    updated project

--- templates/customer_template.php
+++ templates/customer_template.php
@@ -10,11 +10,59 @@
 
 <body>
 
-  <div class="order-heading">
-    <h1>Orders</h1>
-  </div>
+  <table class="table-sortable" id="stock">
+    <thead id="list-column">
+      <tr>
+        <th>Name</th>
+        <th>Nickname</th>
+        <th>Address</th>
+        <th>Phone</th>
+        <th>ID</th>
+        <th>Options</th>
+      </tr>
+    </thead>
 
-  <div class="container">
+    <tbody id="root-stock" class="list-items <?php echo $theme ?>">
+      <?php
+      include("./connect.php");
+      error_reporting(0);
+      $query = "SELECT * FROM customer";
+      $data = mysqli_query($connect, $query);
+      $total = mysqli_num_rows($data);
+
+      if ($total != 0) {
+        while ($result = mysqli_fetch_assoc($data)) {
+          echo
+          "
+          <tr>
+          <td>" . $result['Customer_Name'] . "</td>
+          <td>" . $result['Nickname'] . "</td>
+          <td>" . $result['Location'] . "</td>
+          <td>" . $result['Phone'] . "</td>
+          <td>" . $result['Customer_ID'] . "</td>
+          <td><a href='modify_customers.php?cname=$result[Customer_Name]&nickname=$result[Nickname]&phone=$result[Phone]&cid=$result[Customer_ID]' class='btnModify'>Modify</td>
+          </tr>
+          ";
+        }
+      } else {
+        echo "No records found";
+      }
+      ?>
+
+    </tbody>
+
+    <!-- <tfoot>
+      <tr>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th>Total</th>
+        <th id="result"></th>
+        <th colspan="2"></th>
+      </tr>
+    </tfoot> -->
+  </table>
+
 </body>
 
 </html>

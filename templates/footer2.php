5b8f706e7c91ab31ed1165accaeb422d385eb8b6
Abid Al-Wasi <abidalwasi@outlook.com>
2021-07-12 23:16:48 +0600

    updated project

--- templates/footer2.php
+++ templates/footer2.php
@@ -5,8 +5,27 @@
     var item = document.querySelectorAll(".value.num.item");
     var times = document.querySelectorAll(".value.num.times");
 
+    a = item[0].textContent;
+    b = times[0].textContent;
 
-    console.log(a.textContent);
+    c = item[1].textContent;
+    d = times[1].textContent;
+
+    e = item[2].textContent;
+    f = times[2].textContent;
+
+    console.log(a);
+    console.log(b);
+
+    console.log(c);
+    console.log(d);
+
+    console.log(e);
+    console.log(f);
+
+    console.log(a * b);
+    console.log(c * d);
+    console.log(e * f);
   }
 </script>
 
